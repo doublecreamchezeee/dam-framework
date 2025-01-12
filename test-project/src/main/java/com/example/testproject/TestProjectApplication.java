7c2fe4cb6c32693d7835bcdcb9c7436c178b4e96
“purified-water” <julianng1110@gmail.com>
2025-01-12 22:38:57 +0700

    feat: update test project

--- test-project/src/main/java/com/example/testproject/TestProjectApplication.java
+++ test-project/src/main/java/com/example/testproject/TestProjectApplication.java
@@ -22,9 +22,9 @@ import java.util.Map;
 public class TestProjectApplication {
     public static void main(String[] args) {
         // Call the desired test function
-        testPostgres();
-//        testMySQL();
-//        testNeo4j();
+        // testPostgres();
+        // testMySQL();
+        testNeo4j();
     }
 
     private static void testPostgres() {
@@ -41,43 +41,48 @@ public class TestProjectApplication {
                 .build();
 
         try {
-            ConnectionManager.configureDatasource(configuration, new SqlConnectionFactory(), new PostgresSQLDatabaseConnector());
+            ConnectionManager.configureDatasource(configuration, new SqlConnectionFactory(),
+                    new PostgresSQLDatabaseConnector());
             System.out.println("Connected to PostgreSQL database");
 
             DatabaseManagerAbstractFactory factory = ConnectionManager.createRecordManagerFactory("simple-dao");
             DatabaseOperationManager databaseManager = factory.createRecordManager();
 
-            System.out.println("Insert user");
-            // Perform database operations
-            QueryBuilder insertQuery = QueryBuilder.insert("user")
-                    .value("id", 1)
-                    .value("name", "PostgresUser");
-            databaseManager.executeUpdate(insertQuery);
+            // System.out.println("INSERT USER");
+            // // Perform database operations
+            // QueryBuilder insertQuery = QueryBuilder.insert("user")
+            //         .value("id", 4)
+            //         .value("name", "Anh Tuan");
+            // databaseManager.executeUpdate(insertQuery);
 
-            System.out.println("Update user");
-            // UPDATE user SET name = 'atuan' WHERE id = 1
-            // If not exist then INSERT
-             QueryBuilder query = QueryBuilder.update("user")
-                 .setter("name", "Trithanh")
-                 .where("id = 2");
-             databaseManager.executeUpdate(query);
+            // System.out.println("UPDATE USER");
+            // // UPDATE user SET name = 'atuan' WHERE id = 1
+            // // If not exist then INSERT
+            // QueryBuilder query = QueryBuilder.update("user")
+            // .setter("name", "Trithanh")
+            // .where("id = 1");
+            // databaseManager.executeUpdate(query);
 
-            List<User> users1 = databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
-            users1.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
+            // List<User> users1 = databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
+            // users1.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
-            System.out.println("Select groupby");
-            // SELECT * FROM user WHERE id = 1
-             QueryBuilder selectQuery = QueryBuilder.select()
-                 .from("user")
-                 .groupBy("id, name")
-                 .having("id = 1");
-             List<User> users2 = databaseManager.executeQuery(selectQuery, User.class);
-             users2.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
+            // System.out.println("SELECT GROUP BY");
+            // // SELECT * FROM user WHERE id = 1
+            // QueryBuilder selectQuery = QueryBuilder.select()
+            // .from("user")
+            // .groupBy("id, name")
+            // .having("name = 'Anh Tuan'");
+            // List<User> users2 = databaseManager.executeQuery(selectQuery, User.class);
+            // users2.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
+            System.out.println("DELETE USER");
             // DELETE FROM user WHERE id = 1
-             QueryBuilder deleteQuery = QueryBuilder.delete("user")
-                 .where("id = 1");
-             databaseManager.executeUpdate(deleteQuery);
+            QueryBuilder deleteQuery = QueryBuilder.delete("user")
+            .where("id = 4");
+            databaseManager.executeUpdate(deleteQuery);
+            List<User> users3 =
+            databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
+            users3.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
             ConnectionManager.release("simple-dao");
         } catch (Exception e) {
@@ -99,43 +104,49 @@ public class TestProjectApplication {
                 .build();
 
         try {
-            ConnectionManager.configureDatasource(configuration, new SqlConnectionFactory(), new MySQLDatabaseConnector());
+            ConnectionManager.configureDatasource(configuration, new SqlConnectionFactory(),
+                    new MySQLDatabaseConnector());
             System.out.println("Connected to MySQL database");
 
             DatabaseManagerAbstractFactory factory = ConnectionManager.createRecordManagerFactory("simple-dao");
             DatabaseOperationManager databaseManager = factory.createRecordManager();
 
             // Perform database operations
-            QueryBuilder insertQuery = QueryBuilder.insert("user")
-                    .value("id", 1)
-                    .value("name", "MySQLUser");
-            databaseManager.executeUpdate(insertQuery);
+            // System.out.println("INSERT USER");
+            // QueryBuilder insertQuery = QueryBuilder.insert("user")
+            // .value("id", 3)
+            // .value("name", "Anh Tuan");
+            // databaseManager.executeUpdate(insertQuery);
 
-            System.out.println("Update user");
-            // UPDATE user SET name = 'atuan' WHERE id = 1
-            // If not exist then INSERT
-            QueryBuilder query = QueryBuilder.update("user")
-                    .setter("name", "Trithanh")
-                    .where("id = 2");
-            databaseManager.executeUpdate(query);
+            // System.out.println("UPDATE USER");
+            // // UPDATE user SET name = 'atuan' WHERE id = 1
+            // // If not exist then INSERT
+            // QueryBuilder query = QueryBuilder.update("user")
+            // .setter("name", "Trithanh")
+            // .where("id = 3");
+            // databaseManager.executeUpdate(query);
 
-            List<User> users1 = databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
-            users1.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
+            // List<User> users1 =
+            // databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
+            // users1.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
-            System.out.println("Select groupby");
-            // SELECT * FROM user WHERE id = 1
-            QueryBuilder selectQuery = QueryBuilder.select()
-                    .from("user")
-                    .groupBy("id, name")
-                    .having("id = 1");
-            List<User> users2 = databaseManager.executeQuery(selectQuery, User.class);
-            users2.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
+            // System.out.println("SELECT GROUP BY");
+            // // SELECT * FROM user WHERE id = 1
+            // QueryBuilder selectQuery = QueryBuilder.select()
+            // .from("user")
+            // .groupBy("id, name")
+            // .having("name = 'MySQLUser'");
+            // List<User> users2 = databaseManager.executeQuery(selectQuery, User.class);
+            // users2.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
+            System.out.println("DELETE USER");
             // DELETE FROM user WHERE id = 1
             QueryBuilder deleteQuery = QueryBuilder.delete("user")
-                    .where("id = 1");
+            .where("id = 1");
             databaseManager.executeUpdate(deleteQuery);
-
+            List<User> users1 =
+            databaseManager.executeQuery(QueryBuilder.select().from("user"), User.class);
+            users1.forEach(u -> System.out.println(u.getId() + " " + u.getName()));
 
             ConnectionManager.release("simple-dao");
         } catch (Exception e) {
@@ -156,50 +167,57 @@ public class TestProjectApplication {
                 .build();
 
         try {
-            ConnectionManager.configureDatasource(configuration, new NoSqlConnectionFactory(), new Neo4jDatabaseConnector());
+            ConnectionManager.configureDatasource(configuration, new NoSqlConnectionFactory(),
+                    new Neo4jDatabaseConnector());
             System.out.println("Connected to Neo4j database");
             DatabaseManagerAbstractFactory factory = ConnectionManager.createRecordManagerFactory("simple-dao");
             DatabaseOperationManager databaseManager = factory.createRecordManager();
 
-//            // Perform database operations
-//            User user = new User(1, "Neo4jUser");
-//            databaseManager.insert(user);
-//
-//            ResultSet users = databaseManager.executeQuery(QueryBuilder.select().from("user"));
-//            while (users.next()) {
-//                System.out.println(users.getInt("id"));
-//                System.out.println(users.getString("name"));
-//            }
-//
-//            System.out.println("Update user");
-//            // UPDATE user SET name = 'atuan' WHERE id = 1
-//            // If not exist then INSERT
-//            QueryBuilder query = QueryBuilder.update("user")
-//                    .setter("name", "Trithanh")
-//                    .where("id = 2");
-//            databaseManager.executeUpdate(query);
-//
-//            ResultSet users1 = databaseManager.executeQuery(QueryBuilder.select().from("user"));
-//            while (users1.next()) {
-//                System.out.println(users1.getInt("id"));
-//                System.out.println(users1.getString("name"));
-//            }
+            // Perform database operations
+            // System.out.println("INSERT USER");
+            // User user = new User(4, "Anh Tuan");
+            // databaseManager.insert(user);
 
-            System.out.println("Select groupby");
-            // SELECT * FROM user WHERE id = 1
-            QueryBuilder selectQuery = QueryBuilder.select()
-                    .from("user")
-                    .groupBy("id, name")
-                    .having("id = 1");
-            ResultSet users2 = databaseManager.executeQuery(QueryBuilder.select().from("user"));
-            while (users2.next()) {
-                System.out.println(users2.getInt("id"));
-                System.out.println(users2.getString("name"));
-            }
+            // ResultSet users =
+            // databaseManager.executeQuery(QueryBuilder.select().from("user"));
+            // while (users.next()) {
+            // System.out.println(users.getInt("id"));
+            // System.out.println(users.getString("name"));
+            // }
+
+            // System.out.println("UPDATE USER");
+            // // UPDATE user SET name = 'atuan' WHERE id = 1
+            // // If not exist then INSERT
+            // databaseManager.beginTransaction();
+            // QueryBuilder query = QueryBuilder.update("user")
+            // .setter("name", "Trithanh")
+            // .where("id = 2");
+            // databaseManager.executeUpdate(query);
+
+            // ResultSet users1 =
+            // databaseManager.executeQuery(QueryBuilder.select().from("user"));
+            // while (users1.next()) {
+            // System.out.println(users1.getInt("id"));
+            // System.out.println(users1.getString("name"));
+            // }
+            // databaseManager.commitTransaction();
+
+            // System.out.println("SELECT WHERE");
+            // // SELECT * FROM user WHERE name = 'Neo4jUser'
+            // QueryBuilder selectQuery = QueryBuilder.select()
+            //         .from("user")
+            //         .where("name = 'Neo4jUser'");
+            // ResultSet users2 = databaseManager.executeQuery(selectQuery);
+            // while (users2.next()) {
+            //     System.out.println(users2.getInt("id"));
+            //     System.out.println(users2.getString("name"));
+            // }
+
+            System.out.println("DELETE USER");
             databaseManager.beginTransaction();
             // DELETE FROM user WHERE id = 1
             QueryBuilder deleteQuery = QueryBuilder.delete("user")
-                    .where("id = 1");
+            .where("id = 1");
             databaseManager.executeUpdate(deleteQuery);
             databaseManager.commitTransaction();
 
